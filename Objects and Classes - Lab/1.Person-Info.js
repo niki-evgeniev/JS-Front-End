1e77e19ef4c7d97b3881ef789da02700e43ad94e
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-14 00:26:19 +0200

    Create 1.Person-Info.js

---
+++ Objects and Classes - Lab/1.Person-Info.js
@@ -0,0 +1,11 @@
+function person(firstName, lastName, age) {
+// let person = {firstName, lastName,age};
+let person = {firstname: firstName, lastName: lastName, age: age};
+return person;
+}
+
+
+console.log(person("Peter", 
+"Pan",
+"20"
+));
