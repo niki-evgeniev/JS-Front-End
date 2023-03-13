ad371917bb2cef8423ab8cbd5ad18c8dde147098
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-14 00:26:29 +0200

    Create 6.Phone-book.js

---
+++ Objects and Classes - Lab/6.Phone-book.js
@@ -0,0 +1,12 @@
+function phoneBook(info){
+    console.log(info);
+
+    const book = info.split(' ');
+    console.log(book);
+}
+
+phoneBook(['Tim 0834212554',
+'Peter 0877547887',
+'Bill 0896543112',
+'Tim 0876566344']
+)
