c0e5747df7056fab4b5ffa1798ba586bcafcb96c
Nikolay Ivanov <niki.evgeniev@gmail.com>
2023-03-14 00:25:23 +0200

    Create 1.Exercise-Functions-and-Statements.js

---
+++ Exercise Functions and Statements/1.Exercise-Functions-and-Statements.js
@@ -0,0 +1,26 @@
+function smallNumber(num1, num2, num3) {
+    let smallNum = Math.min(num1, num2, num3);
+
+    console.log(smallNum);
+}
+
+
+smallNumber(2,
+    5,
+    3
+);
+
+smallNumber(600,
+    342,
+    123
+);
+
+smallNumber(25,
+    21,
+    4
+);
+
+smallNumber(2,
+    2,
+    2
+);
